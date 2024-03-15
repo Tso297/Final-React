0472f08f2140ed0f546a1c049b82d21aeb6de065
Travis <you@example.com>
2024-03-15 12:44:01 -0400

    fix auth

--- src/auth/AuthChecker.tsx
+++ src/auth/AuthChecker.tsx
@@ -1,18 +1,17 @@
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { signInWithPopup } from 'firebase/auth'
-import{ auth, Providers } from '../config/firebase'
+
 
 interface Props {
     children: React.ReactNode;
 }
 
 const AuthChecker = ({ children }:Props) => {
+    const isSignedIn = localStorage.getItem('isSignedIn')
     const navigate = useNavigate();
     useEffect(() => {
-        if (!auth.currentUser) {
+        if (!isSignedIn) {
             navigate('../')
-            signInWithPopup(auth, Providers.google)
         }
     }, [])
 
